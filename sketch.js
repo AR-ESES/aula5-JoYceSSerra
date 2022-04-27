5a9463837d72c883423ecbd4e25e131e70c0b776
JoYceSSerra <94360123+JoYceSSerra@users.noreply.github.com>
2022-04-27 10:40:28 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,16 +2,28 @@ let laranjas;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(0)
+	background(0);
 
 }
 
 function draw() {
 
+	background(0)
+
+	if(mouseY>=height/2 && mouseX<=width/2)  
+    fill	(0,0,128); // azul
+
+    else if(mouseY<height/2 && mouseX<=width/2) 
+    fill	(255,0,0); // vermelho
+
+    else if(mouseY<height/2 && mouseX>width/2) 
+    fill	(0,128,0); // verde 
+
+	else{
+		 noFill();
+	}
+
 	stroke(230);
 	strokeWeight(2);
-	noFill();
 	circle(width / 2, height / 2, height / 4);
-
-
 }
